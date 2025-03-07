f445cf494345ab6a09cdeb4efef1316683a442f4
Ashok-Maruvada <ashokmaruvada6174@gmail.com>
2025-03-07 16:51:18 +0530

    practice

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "public_ip" {
-    value = module.ec2-test.public_ip
+    value = module.ec2-test[each.key].public_ip
 }
 
 output "private_ip" {
-    value = module.ec2-test.private_ip
+    value = module.ec2-test[each.key].private_ip
 }
