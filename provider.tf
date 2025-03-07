8080a0ffb32a28d450978e12a87553767a7fe818
Ashok-Maruvada <ashokmaruvada6174@gmail.com>
2025-03-07 16:39:29 +0530

    practice

--- provider.tf
+++ provider.tf
@@ -6,6 +6,12 @@ terraform {
       #provider-version
     }
   }
+  backend "s3" {
+    bucket = "ashok-remote-state"
+    key    = "ec2-module"
+    region = "us-east-1"
+    dynamodb_table = "ashok-locking"
+  }
 }
 
 provider "aws" {
