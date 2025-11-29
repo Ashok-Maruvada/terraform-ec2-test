f7f5c3f5269386c15b073b5591b4da951b6ecff0
Ashok-Maruvada <ashokmaruvada6174@gmail.com>
2025-11-29 11:52:29 +0530

    adding workflow

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 module "ec2-test" {
-    source = "../terraform-aws-ec2"
+    #source = "../terraform-aws-ec2"
+    source = "https://github.com/Ashok-Maruvada/terraform-aws-ec2.git"
     #we can also pass variables inside modules which can ovrride default variables
-
 }
