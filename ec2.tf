9ac92fc735b6c334fc3806a10d7405bbf2ffdc3e
M Ashok <167614394+Ashok-Maruvada@users.noreply.github.com>
2025-11-29 13:02:05 +0530

    merging feat/infra to main (#1)
    * created feature
    
    * adding permission block
    
    * updated source

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 module "ec2-test" {
     #source = "../terraform-aws-ec2"
-    source = "https://github.com/Ashok-Maruvada/terraform-aws-ec2.git"
+    source = "git::https://github.com/Ashok-Maruvada/terraform-aws-ec2.git?ref=main"
     #we can also pass variables inside modules which can ovrride default variables
 }
--- variables.tf
+++ variables.tf
@@ -2,6 +2,7 @@ variable "instance_type" {
     #default = "t3.micro"
     type = map
     default = {
-        
+        monitoring="t3.micro"
+        node="t3.micro"
     }
 }
